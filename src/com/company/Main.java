8b3401899c18cf553742ead67f7b16c5b4b6c2f1
unknown <you@example.com>
2015-11-07 13:43:15 +0100

    zmiana kodowania

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("WZOR NA OBLICZANIE POLA TRAPEZU");
+        int a , b , h , wynik ;//inicjujemy wartoœci zmienne a,b,h oraz wynik
+        //nastêpnie okreœlamy wartoœci dla zmiennygh
+        a = 6 ;
+        b = 8 ;
+        h = 3 ;
+        wynik = (a+b)/2*h ; // okreœlamy formu³ê realizuj¹c¹ nasz wzór
+        System.out.println(wynik);
+        System.out.println("DZIALA");
+	// write your code here
+    }
+}
